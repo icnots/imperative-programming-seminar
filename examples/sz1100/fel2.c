23bfcd3983da12c875af8e821364db3ea1ff9a9f
Agoston Kaposi <55279434+icnots@users.noreply.github.com>
2023-09-27 16:32:42 +0200

    Add files via upload

---
+++ examples/sz1100/fel2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+    int a, b, c;
+    a = 0;
+    b = 3;
+    c = 2;
+
+    if ((a < b) && (b < c)){
+        printf("True\n");
+    }else{
+        printf("False\n");
+    }
+
+    return 0;
+}
+
+
+
